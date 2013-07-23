64a27fd8edeb90967f8e5cfc70f3d752d29c9298
Timothy J. Warren <tim@timshomepage.net>
2013-07-23 08:36:28 -0400

    Fix incorrect url for live bookmarklet

--- LazyFormFuzz-live.js
+++ LazyFormFuzz-live.js
@@ -1,1 +1,1 @@
-javascript:(function(b){var a=document.createElement("script");a.src="//github.com/aviat4ion/LazyFormFuzz/blob/master/LazyFormFuzz.js";a.async=!1;document.getElementsByTagName("head")[0].appendChild(a)})();
+javascript:(function(b){var a=document.createElement("script");a.src="https://raw.github.com/aviat4ion/LazyFormFuzz/master/LazyFormFuzz.js";a.async=!1;document.getElementsByTagName("head")[0].appendChild(a)})();
